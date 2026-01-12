a1660e2085c700b8f4305fdb8f9574a525bac16e
revurii <koukuyou.mi@gmail.com>
2026-01-12 23:03:46 +0800

    solar panel textures

---
+++ assets/gregtech/textures/blocks/copy solarpanel.bat
@@ -0,0 +1,14 @@
+pause
+
+copy /y iconsets\SOLARPANEL.png iconsets\SOLARPANEL_8V.png
+copy /y iconsets\SOLARPANEL.png iconsets\SOLARPANEL_LV.png
+copy /y iconsets\SOLARPANEL.png iconsets\SOLARPANEL_MV.png
+copy /y iconsets\SOLARPANEL.png iconsets\SOLARPANEL_HV.png
+copy /y iconsets\SOLARPANEL.png iconsets\SOLARPANEL_EV.png
+copy /y iconsets\SOLARPANEL.png iconsets\SOLARPANEL_IV.png
+copy /y iconsets\SOLARPANEL.png iconsets\SOLARPANEL_LuV.png
+copy /y iconsets\SOLARPANEL.png iconsets\SOLARPANEL_ZPM.png
+copy /y iconsets\SOLARPANEL.png iconsets\SOLARPANEL_UV.png
+copy /y iconsets\SOLARPANEL.png iconsets\SOLARPANEL_UHV.png
+copy /y iconsets\SOLARPANEL.png iconsets\SOLARPANEL_UEV.png
+copy /y iconsets\SOLARPANEL.png iconsets\SOLARPANEL_UIV.png
